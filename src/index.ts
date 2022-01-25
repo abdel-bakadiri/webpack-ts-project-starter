21ebd6042c93e50f6c53ba60f6d4167a8a0e0dd8
abdel <abdelouahab.bakadiri@gmail.com>
2022-01-25 16:04:14 +0100

    Hot observable: multicasting

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
-import { ajax, AjaxResponse } from "rxjs/ajax";
-const ajax$ = ajax<any>("https://random-data-api.com/api/name/random_name");
-ajax$.subscribe((data) => {
-  console.log("Subscription 1");
-  console.log(data.response.name);
+import { Observable } from "rxjs";
+
+const clickedButton = document.querySelector("button#hot-observable");
+const hotObservable$ = new Observable<any>((subscriber) => {
+  clickedButton.addEventListener("click", (event) => subscriber.next(event));
 });
-ajax$.subscribe((data) => {
-  console.log("Subscription 2");
-  console.log(data.response.name);
-});
-ajax$.subscribe((data) => {
-  console.log("Subscription 3");
-  console.log(data.response.name);
-});
+hotObservable$.subscribe((clickedButtonEvent) =>
+  console.log(
+    "subscription 1",
+    clickedButtonEvent.type,
+    clickedButtonEvent.x,
+    clickedButtonEvent.y
+  )
+);
+setTimeout(
+  () => {
+    console.log("Subscription 2 starts");
+    hotObservable$.subscribe((clickedButtonEvent) =>
+      console.log(
+        "subscription 2",
+        clickedButtonEvent.type,
+        clickedButtonEvent.x,
+        clickedButtonEvent.y
+      )
+    );
+  },
+
+  5000
+);
