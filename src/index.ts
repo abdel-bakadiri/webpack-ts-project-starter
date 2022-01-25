fc75730d867faccc636e700315ee7be6ff1d0dd4
abdel <abdelouahab.bakadiri@gmail.com>
2022-01-25 15:43:29 +0100

    subscriptions to a cold observable

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-import { ajax } from "rxjs/ajax";
-ajax("https://random-data-api.com/api/name/random_name").subscribe((data) =>
-  console.log(data)
-);
+import { ajax, AjaxResponse } from "rxjs/ajax";
+const ajax$ = ajax<any>("https://random-data-api.com/api/name/random_name");
+ajax$.subscribe((data) => {
+  console.log("Subscription 1");
+  console.log(data.response.name);
+});
+ajax$.subscribe((data) => {
+  console.log("Subscription 2");
+  console.log(data.response.name);
+});
+ajax$.subscribe((data) => {
+  console.log("Subscription 3");
+  console.log(data.response.name);
+});
