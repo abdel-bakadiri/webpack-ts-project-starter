d65379fd80bafeda29cfce6f100a4dfdcaee9dd8
abdel <abdelouahab.bakadiri@gmail.com>
2022-01-26 21:50:03 +0100

    convertiseur C to F using combinneLatest rxjs function

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,23 @@
-import { forkJoin, Observable } from "rxjs";
-
-const observable1$ = new Observable((subscriber) => {
-  setTimeout(() => {
-    subscriber.next("A");
-    subscriber.complete();
-  }, 3000);
-  return () => {
-    console.log("teardown observable 1");
-  };
-});
-const observable2$ = new Observable((subscriber) => {
-  setTimeout(() => {
-    subscriber.error(new Error("404"));
-  }, 5000);
-  return () => {
-    console.log("teardown observable 2");
-  };
-});
-forkJoin([observable1$, observable2$]).subscribe({
-  next: (value) => {
-    console.log(value);
-  },
-  error: (err) => console.log(err.message),
-});
+import { forkJoin, fromEvent, Observable } from "rxjs";
+import { combineLatest } from "rxjs/internal/observable/combineLatest";
+const temperatureInput = document.querySelector("input#temperature-input");
+const conversionDropDownInput = document.querySelector(
+  "select#conversion-dropdown"
+);
+const resultText = document.querySelector("p#result-text");
+const temperature$ = fromEvent<any>(temperatureInput, "input");
+const conversion$ = fromEvent<any>(conversionDropDownInput, "input");
+combineLatest([temperature$, conversion$]).subscribe(
+  ([temepratureInputEvent, conversionInputEvane]) => {
+    const temperature = Number(temepratureInputEvent.target["value"]);
+    const convertionType = conversionInputEvane.target["value"];
+    let result;
+    if (convertionType === "f-to-c") {
+      result = ((temperature - 32) * 5) / 9;
+    }
+    if (convertionType === "c-to-f") {
+      result = (temperature * 9) / 5 + 32;
+    }
+    resultText.innerHTML = String(result);
+  }
+);
