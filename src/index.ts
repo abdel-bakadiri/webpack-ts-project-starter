219fea996992242e0244398418e4befd120d52a0
abdel <abdelouahab.bakadiri@gmail.com>
2022-01-26 13:20:05 +0100

    using fromEvent to triggerr an event

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import { from } from "rxjs";
-const promise = new Promise((resolve, reject) => {
-  reject("Error");
+import { fromEvent, Observable } from "rxjs";
+// using fromEvent to triger event each time the button is clicked
+const buttonFromEvent = document.querySelector("button#hot-observable");
+fromEvent<MouseEvent>(buttonFromEvent, "click").subscribe((event) => {
+  console.log(event.type, event.x, event.y);
 });
-from(promise).subscribe({
-  next: (value) => console.log(value),
-  error: (error) => console.log(error),
-  complete: () => console.log("Complete"),
-});
+// using addEventListner to triger event each time the button is clicked
+// const observable$ = new Observable<MouseEvent>((subscriber) => {
+//   buttonFromEvent.addEventListener("click", (eventClick: MouseEvent) => {
+//     subscriber.next(eventClick);
+//   });
+// });
+// observable$.subscribe((eventClick) =>
+//   console.log(eventClick.type, eventClick.x, eventClick.y)
+// );
