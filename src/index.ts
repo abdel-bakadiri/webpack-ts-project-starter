9538749ed35d29180a519c803bb69301db6e9752
abdel <abdelouahab.bakadiri@gmail.com>
2022-01-26 15:57:43 +0100

    intervall rxjs  function

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,16 @@
-import { Observable, timer } from "rxjs";
-// timer(5000).subscribe({
-//   next: (value) => {
-//     console.log(value);
-//   },
-//   complete: () => console.log("complete"),
-// });
-const myTimer$ = new Observable<number>((subscriber) => {
-  const timeoutId = setTimeout(() => {
-    console.log("timeout");
-    subscriber.next(0);
-    subscriber.complete();
-  }, 5000);
-  // teardown logic to clen up
-  return () => clearTimeout(timeoutId);
+import { interval, Observable } from "rxjs";
+// const interval$ = interval(1000);
+// const subscription = interval$.subscribe((value) => console.log(value));
+// setTimeout(() => {
+//   console.log("Start unsubscription");
+//   subscription.unsubscribe();
+// }, 5000);
+
+const myInterval$ = new Observable((subscriber) => {
+  let digit = 0;
+  const idInterval = setInterval(() => {
+    subscriber.next(digit++);
+  }, 1000);
+  return () => clearInterval(idInterval);
 });
-const subscription = myTimer$.subscribe({
-  next: (value) => {
-    console.log(value);
-  },
-  complete: () => console.log("complete !"),
-});
-setTimeout(() => {
-  subscription.unsubscribe();
-}, 2000);
+myInterval$.subscribe((digitValue) => console.log(digitValue));
