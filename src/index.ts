be370e466e1d4134803e6e47483916d7e82b3f06
abdel <abdelouahab.bakadiri@gmail.com>
2022-01-26 13:21:12 +0100

    using to addEventListner to simulate fromEvent from rxjs

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { fromEvent, Observable } from "rxjs";
 // using fromEvent to triger event each time the button is clicked
 const buttonFromEvent = document.querySelector("button#hot-observable");
-fromEvent<MouseEvent>(buttonFromEvent, "click").subscribe((event) => {
-  console.log(event.type, event.x, event.y);
-});
-// using addEventListner to triger event each time the button is clicked
-// const observable$ = new Observable<MouseEvent>((subscriber) => {
-//   buttonFromEvent.addEventListener("click", (eventClick: MouseEvent) => {
-//     subscriber.next(eventClick);
-//   });
+// fromEvent<MouseEvent>(buttonFromEvent, "click").subscribe((event) => {
+//   console.log(event.type, event.x, event.y);
 // });
-// observable$.subscribe((eventClick) =>
-//   console.log(eventClick.type, eventClick.x, eventClick.y)
-// );
+// using addEventListner to triger event each time the button is clicked
+const observable$ = new Observable<MouseEvent>((subscriber) => {
+  buttonFromEvent.addEventListener("click", (eventClick: MouseEvent) => {
+    subscriber.next(eventClick);
+  });
+});
+observable$.subscribe((eventClick) =>
+  console.log(eventClick.type, eventClick.x, eventClick.y)
+);
