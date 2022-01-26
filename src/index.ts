ff5df3daa60732505f9beaaac1da35f61c3fadb4
abdel <abdelouahab.bakadiri@gmail.com>
2022-01-26 13:44:35 +0100

    when we subscrib to the fromEvent the notifications stop

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 import { fromEvent, Observable } from "rxjs";
 // using fromEvent to triger event each time the button is clicked
 const buttonFromEvent = document.querySelector("button#hot-observable");
-// fromEvent<MouseEvent>(buttonFromEvent, "click").subscribe((event) => {
-//   console.log(event.type, event.x, event.y);
-// });
-// using addEventListner to triger event each time the button is clicked
-const observable$ = new Observable<MouseEvent>((subscriber) => {
-  buttonFromEvent.addEventListener("click", (eventClick: MouseEvent) => {
-    subscriber.next(eventClick);
-  });
-});
-observable$.subscribe((eventClick) =>
-  console.log(eventClick.type, eventClick.x, eventClick.y)
+const fromEvent$ = fromEvent<MouseEvent>(buttonFromEvent, "click").subscribe(
+  (event) => {
+    console.log(event.type, event.x, event.y);
+  }
 );
+setTimeout(() => {
+  console.log("unsubscribe begin");
+  fromEvent$.unsubscribe();
+}, 5000);
+// using addEventListner to triger event each time the button is clicked
+// const observable$ = new Observable<MouseEvent>((subscriber) => {
+//   const handleFn = (eventClick: MouseEvent) => {
+//     console.log(" Teardown ");
+//     subscriber.next(eventClick);
+//   };
+//   buttonFromEvent.addEventListener("click", handleFn);
+//   return () => {
+//     buttonFromEvent.removeEventListener("click", handleFn);
+//   };
+// });
+// const subscription = observable$.subscribe((eventClick) =>
+//   console.log(eventClick.type, eventClick.x, eventClick.y)
+// );
+// setTimeout(() => {
+//   subscription.unsubscribe();
+// }, 5000);
