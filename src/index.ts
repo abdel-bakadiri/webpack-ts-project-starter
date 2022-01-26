b48056264f0dadb9dc28ecf91e7164d7f65946c8
abdel <abdelouahab.bakadiri@gmail.com>
2022-01-26 21:13:39 +0100

    forkJoin with error

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
-import { forkJoin } from "rxjs";
-import { ajax } from "rxjs/ajax";
-const perso$ = ajax("https://random-data-api.com/api/name/random_name");
-const city$ = ajax("https://random-data-api.com/api/nation/random_nation");
-const food$ = ajax("https://random-data-api.com/api/food/random_food");
-// forkJoin([perso$, city$, food$]).subscribe(([person, city, food]) =>
-//   console.log(`Mr ${person}`)
-// );
-forkJoin<any>([perso$, city$, food$]).subscribe(([person, city, food]) =>
-  console.log(
-    `${person.response.first_name} live in ${city.response.capital} and like to eat ${food.response.dish}`
-  )
-);
+import { forkJoin, Observable } from "rxjs";
 
-// first_name
-//  capital
-// dish
+const observable1$ = new Observable((subscriber) => {
+  setTimeout(() => {
+    subscriber.next("A");
+    subscriber.complete();
+  }, 3000);
+  return () => {
+    console.log("teardown observable 1");
+  };
+});
+const observable2$ = new Observable((subscriber) => {
+  setTimeout(() => {
+    subscriber.error(new Error("404"));
+  }, 5000);
+  return () => {
+    console.log("teardown observable 2");
+  };
+});
+forkJoin([observable1$, observable2$]).subscribe({
+  next: (value) => {
+    console.log(value);
+  },
+  error: (err) => console.log(err.message),
+});
