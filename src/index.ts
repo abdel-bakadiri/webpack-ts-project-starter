99227a1ef66e2ed2da80397a6ee66ef333838be4
abdel <abdelouahab.bakadiri@gmail.com>
2022-01-29 20:43:17 +0100

    use debounce time pipeable operators

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,10 @@
-import { concat, Observable, of } from "rxjs";
-import { filter, map, tap } from "rxjs/operators";
+import { fromEvent } from "rxjs";
+import { debounceTime, map } from "rxjs/operators";
+const slider = document.querySelector("input#slider");
 
-concat(
-  of(1, 2, 3, 4, 5, 6),
-  new Observable((subscriber) => {
-    setTimeout(() => subscriber.error(new Error("404")), 5000);
-  })
-)
+fromEvent<any>(slider, "input")
   .pipe(
-    tap((value) => console.log("original value", value)),
-    map((value) => (typeof value === "number" ? value * 2 : value)),
-    tap((value) => console.log("maped value", value)),
-    filter((value) => value > 3),
-    tap({
-      next: (value) => console.log(value),
-      error: (err) => console.log("Error happened", err.message),
-      complete: () => console.log("complete subscription"),
-    })
+    debounceTime(3000),
+    map((event) => event.target["value"])
   )
-  .subscribe((value) => console.log("Resultat value", value));
+  .subscribe((inputValue) => console.log(inputValue));
