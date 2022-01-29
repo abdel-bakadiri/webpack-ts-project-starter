ac8dd0118351ad55841671f379a322fb59444983
abdel <abdelouahab.bakadiri@gmail.com>
2022-01-29 21:55:49 +0100

    use catchError pipeable operator to catcherror

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import { fromEvent } from "rxjs";
-import { debounceTime, map } from "rxjs/operators";
-const slider = document.querySelector("input#slider");
+import { catchError, EMPTY, Observable, of } from "rxjs";
 
-fromEvent<any>(slider, "input")
-  .pipe(
-    debounceTime(3000),
-    map((event) => event.target["value"])
-  )
-  .subscribe((inputValue) => console.log(inputValue));
+const httpResquestfailed$ = new Observable((subscriber) => {
+  setTimeout(() => {
+    subscriber.error(new Error("timeout"));
+  }, 3000);
+});
+console.log("application start");
+httpResquestfailed$.pipe(catchError(() => of(1, 2))).subscribe({
+  next: (value) => console.log(value),
+  error: (err) => console.log(err),
+  complete: () => console.log("subscription complete"),
+});
