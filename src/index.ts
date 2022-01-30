0fe9eca61d53f10e6efddac00af777176826a936
abdel <abdelouahab.bakadiri@gmail.com>
2022-01-30 19:39:16 +0100

    use catchError with EMPTY to pass a complete notification instead an error

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
-import { Observable, of } from "rxjs";
-import { concatMap } from "rxjs/operators";
+import { EMPTY, fromEvent } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { catchError, concatMap, map } from "rxjs/operators";
 
-const source$ = new Observable((subscriber) => {
-  setTimeout(() => subscriber.next("A"), 3000);
-  setTimeout(() => subscriber.next("B"), 6000);
-  setTimeout(() => subscriber.next("C"), 9000);
-});
-source$
-  .pipe(concatMap(() => of(1, 2, 3)))
-  .subscribe((value) => console.log(value));
+const endpointInput: HTMLInputElement =
+  document.querySelector("input#endpoint");
+const fetchButton = document.querySelector("button#fetch");
+
+fromEvent(fetchButton, "click")
+  .pipe(
+    map(() => endpointInput.value),
+    concatMap((value) =>
+      ajax(`https://random-data-api.com/api/${value}/random_${value}`)
+    ),
+    catchError(() => EMPTY)
+  )
+  .subscribe({
+    next: (value) => console.log(value),
+    error: (err) => console.log("Error:", err),
+    complete: () => console.log("Completed"),
+  });
